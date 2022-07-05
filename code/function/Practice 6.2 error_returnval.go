2895c68531448924ac50bccad293d80418b39fe3
yu <yu@yu-3.local>
2022-07-05 10:56:34 +0800

    go

---
+++ code/function/Practice 6.2 error_returnval.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var m float64 = 3
+var n float64
+
+func main() {
+	n = MySqrt(m)
+	see()
+	n = MySqrt_2(m)
+	see()
+}
+func see() {
+	fmt.Printf("The sqrt is %f\n ", n)
+}
+func MySqrt(p float64) float64 {
+	return math.Sqrt(p)
+}
+func MySqrt_2(p float64) (q float64) {
+	q = math.Sqrt(p)
+	return
+}
---
+++ code/function/practice 6.1 mult_returnval.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var a, b int = 8, 2
+var x, y, z int
+
+func main() {
+	x, y, z = xyz(a, b)
+	look()
+	x, y, z = xyz_2(a, b)
+	look()
+}
+func look() {
+	fmt.Printf("sum = %d,pro = %d,dif = %d\n", x, y, z)
+}
+func xyz(t1, t2 int) (int, int, int) {
+	return t1 + t2, t1 * t2, t1 - t2
+}
+func xyz_2(t1, t2 int) (x1 int, y1 int, z1 int) {
+	x1 = t1 + t2
+	y1 = t1 * t2
+	z1 = t1 - t2
+	return
+}
