26cace7ca562c68b41c08ec381aac84ef5454816
yu <yu@yu-3.local>
2022-07-05 10:08:56 +0800

    go

---
+++ code/function/6.2 simple_function.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Multiply 2 * 5 * 6 = %d\n", Multipiy3Nums(2, 5, 6))
+}
+func Multipiy3Nums(a int, b int, c int) int {
+	return a * b * c
+}
