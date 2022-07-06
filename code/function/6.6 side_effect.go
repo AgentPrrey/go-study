e20b9480d0e9aab3a0a378093fcd66d76a73ea8d
ch <3568345873@qq.com>
2022-07-06 18:56:21 +0800

    go

--- code/function/6.6 side_effect.go
+++ code/function/6.6 side_effect.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // this function changes reply:
@@ -10,8 +11,12 @@ func Multiply(a, b int, reply *int) {
 }
 
 func main() {
+	Start := time.Now()
 	n := 0
 	reply := &n
 	Multiply(10, 5, reply)
 	fmt.Println("Multiply:", *reply) // Multiply: 50
+	end := time.Now()
+	delta := end.Sub(Start)
+	fmt.Println("The time:", delta)
 }
